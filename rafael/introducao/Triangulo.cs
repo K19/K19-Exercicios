3f54e35bf951cdab11d9087cf39ade1247b4531c
Rafael Cosentino <rafael.cosentino@k19.com.br>
2013-07-27 20:07:31 -0300

    k01-introducao-ex11

---
+++ rafael/introducao/Triangulo.cs
@@ -0,0 +1,11 @@
+class Triangulo 
+{
+	static void Main() 
+	{
+		System.Console.WriteLine("*");
+		System.Console.WriteLine("**");
+		System.Console.WriteLine("***");
+		System.Console.WriteLine("****");
+		System.Console.WriteLine("*****");
+	}
+}
