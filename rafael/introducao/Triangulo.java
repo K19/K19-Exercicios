ef6aaad233f8151a92cbe4574e34a6e8bc361ba1
Rafael Cosentino <rafael.cosentino@k19.com.br>
2013-07-27 19:49:17 -0300

    k01-introducao-ex5

---
+++ rafael/introducao/Triangulo.java
@@ -0,0 +1,9 @@
+class Triangulo {
+	public static void main(String[] args) {
+		System.out.println("*");
+		System.out.println("**");
+		System.out.println("***");
+		System.out.println("****");
+		System.out.println("*****");
+	}
+}
