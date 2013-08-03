b43d045e130a6c88c0211ccb65e905b3715f05bb
Rafael Cosentino <rafael.cosentino@k19.com.br>
2013-08-03 19:43:51 -0300

    k01-variaveis-complementar3

---
+++ rafael/variaveis/TestaTiposBasicos.java
@@ -0,0 +1,21 @@
+class TestaTiposBasicos {
+	public static void main(String[] args) {
+		byte b = 1;
+		short s = 2;
+		int i = 3;
+		long l = 4;
+		float f = 5.5F;
+		double d = 6.6;
+		char c = 'K';
+		boolean v = true;
+
+		System.out.println(b);
+		System.out.println(s);
+		System.out.println(i);
+		System.out.println(l);
+		System.out.println(f);
+		System.out.println(d);
+		System.out.println(c);
+		System.out.println(v);		
+	}
+}
