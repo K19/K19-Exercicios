59fd81457fc6fe372c4a05829de9d3a3041cc900
Rafael Cosentino <rafael.cosentino@k19.com.br>
2013-08-03 19:48:35 -0300

    k01-variaveis-complementar7

---
+++ rafael/variaveis/TestaTiposBasicos.cs
@@ -0,0 +1,33 @@
+class TestaTiposBasicos 
+{
+	static void Main() 
+	{
+		sbyte sb = 1;
+		byte  b = 2;
+		short s = 3;
+		ushort us = 4;
+		int i = 5;
+		uint ui = 6;
+		long l = 7;
+		ulong ul = 8;
+		float f = 5.5F;
+		double d1 = 6.6;
+		decimal d2 = 7.7M;
+		char c = 'K';
+		bool v = true;
+
+		System.Console.WriteLine(sb);
+		System.Console.WriteLine(b);
+		System.Console.WriteLine(s);
+		System.Console.WriteLine(us);
+		System.Console.WriteLine(i);
+		System.Console.WriteLine(ui);
+		System.Console.WriteLine(l);
+		System.Console.WriteLine(ul);
+		System.Console.WriteLine(f);
+		System.Console.WriteLine(d1);
+		System.Console.WriteLine(d2);
+		System.Console.WriteLine(c);
+		System.Console.WriteLine(v);
+	}
+}
