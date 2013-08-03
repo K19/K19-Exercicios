b125f146f934576b916efc61375b3d39232b6b7e
Rafael Cosentino <rafael.cosentino@k19.com.br>
2013-08-03 19:23:53 -0300

    k01-variaveis-fixacao4

---
+++ rafael/variaveis/TestaNumeroAleatorio.java
@@ -0,0 +1,9 @@
+class TestaNumeroAleatorio {
+	public static void main(String[] args) {
+		double numeroAleatorio;
+		
+		numeroAleatorio = Math.random() * 100;
+		
+		System.out.println(numeroAleatorio);
+	}
+}
