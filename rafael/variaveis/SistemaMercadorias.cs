953a8d3afc8afc8a6eae7d7ff5841b2a08a93cb1
Rafael Cosentino <rafael.cosentino@k19.com.br>
2013-08-03 19:52:35 -0300

    k01-variaveis-complementar10

---
+++ rafael/variaveis/SistemaMercadorias.cs
@@ -0,0 +1,15 @@
+class SistemaMercadorias 
+{
+	static void Main() 
+	{
+		int numeroDoPedido = 1523;
+		int codigoDoProduto = 845732;
+		short quantidade = 200;
+		double valorTotalDaCompra = 62373.5;
+		
+		System.Console.WriteLine(numeroDoPedido);
+		System.Console.WriteLine(codigoDoProduto);
+		System.Console.WriteLine(quantidade);
+		System.Console.WriteLine(valorTotalDaCompra);		
+	}
+}
---
+++ rafael/variaveis/SistemaMercadorias.java
@@ -0,0 +1,13 @@
+class SistemaMercadorias {
+	public static void main(String[] args) {
+		int numeroDoPedido = 1523;
+		int codigoDoProduto = 845732;
+		short quantidade = 200;
+		double valorTotalDaCompra = 62373.5;	
+
+		System.out.println(numeroDoPedido);
+		System.out.println(codigoDoProduto);
+		System.out.println(quantidade);
+		System.out.println(valorTotalDaCompra);
+	}
+}
