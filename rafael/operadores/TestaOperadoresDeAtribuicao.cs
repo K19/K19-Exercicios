29639b8434ab01cea955fba41177ddefbf425101
Rafael Cosentino <rafael.cosentino@k19.com.br>
2013-08-10 19:29:08 -0300

    k01-operadores-fixacao28

---
+++ rafael/operadores/TestaOperadoresDeAtribuicao.cs
@@ -0,0 +1,23 @@
+class TestaOperadoresDeAtribuicao 
+{
+	static void Main() 
+	{
+		int a = 1;
+		System.Console.WriteLine(a);
+		
+		a += 2;
+		System.Console.WriteLine(a);
+		
+		a -= 1;
+		System.Console.WriteLine(a);
+
+		a *= 3;
+		System.Console.WriteLine(a);
+
+		a /= 2;
+		System.Console.WriteLine(a);
+		
+		a %= 2;
+		System.Console.WriteLine(a);				
+	}
+}
