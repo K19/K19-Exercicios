00967c073c784bfcf12ac9b86e7c48e3711e4bd7
Rafael Cosentino <rafael.cosentino@k19.com.br>
2013-08-10 19:29:43 -0300

    k01-operadores-fixacao30

---
+++ rafael/operadores/TestaOperadoresRelacionais.cs
@@ -0,0 +1,15 @@
+class TestaOperadoresRelacionais 
+{
+	static void Main() 
+	{
+		int a = 1;
+		int b = 2;
+		
+		System.Console.WriteLine(a > b);
+		System.Console.WriteLine(a >= b);
+		System.Console.WriteLine(a < b);
+		System.Console.WriteLine(a <= b);
+		System.Console.WriteLine(a == b);
+		System.Console.WriteLine(a != b);
+	}
+}
