bdb2c08a7dcb60e40e00b12476f4ee968dab6cd4
Rafael Cosentino <rafael.cosentino@k19.com.br>
2013-08-10 19:41:55 -0300

    k01-operadores-fixacao32

---
+++ rafael/operadores/TestaOperadoresLogicos.cs
@@ -0,0 +1,16 @@
+class TestaOperadoresLogicos 
+{
+	static void Main() 
+	{
+		int a = 1;
+		int b = 2;
+		int c = 3;
+		int d = 4;
+		
+		System.Console.WriteLine(a > b | c < d);
+		System.Console.WriteLine(a > b || c < d);
+		System.Console.WriteLine(a > b & c < d);
+		System.Console.WriteLine(a > b && c < d);
+		System.Console.WriteLine(a > b ^ c < d);
+	}
+}
