365e814a8b02d0cb0c0e7afd3945195e6e0cb2ee
Rafael Cosentino <rafael.cosentino@k19.com.br>
2013-08-10 19:44:31 -0300

    k01-operadores-fixacao38

---
+++ rafael/operadores/TestaPrePosIncrementoDecremento.cs
@@ -0,0 +1,12 @@
+class TestaPrePosIncrementoDecremento 
+{
+	static void Main() 
+	{
+		int a = 1;
+		
+		System.Console.WriteLine(a++);
+		System.Console.WriteLine(++a);
+		System.Console.WriteLine(a--);
+		System.Console.WriteLine(--a);
+	}
+}
