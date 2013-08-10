437ed1053a274ee179e2ca353d3095c33f4f80f0
Rafael Cosentino <rafael.cosentino@k19.com.br>
2013-08-10 19:27:51 -0300

    k01-operadores-fixacao24

---
+++ rafael/operadores/TestaDivisaoInteira.cs
@@ -0,0 +1,14 @@
+class TestaDivisaoInteira 
+{
+	static void Main() 
+	{
+		int a = 5;
+		int b = 2;
+		
+		System.Console.WriteLine(a / b);
+		System.Console.WriteLine((double)a / b);
+		System.Console.WriteLine(a / (double)b);
+		System.Console.WriteLine((double)a / (double)b);
+		System.Console.WriteLine((double)(a / 2));
+	}
+}
