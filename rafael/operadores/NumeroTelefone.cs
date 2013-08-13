e29dac0f57a7023613c67eb646fc13b0e89924fa
Rafael Cosentino <rafael.cosentino@k19.com.br>
2013-08-12 22:34:00 -0300

    k01-operadores-complementar17

---
+++ rafael/operadores/NumeroTelefone.cs
@@ -0,0 +1,20 @@
+class NumeroTelefone
+{
+	static void Main()
+	{
+		int x = 23;
+		System.Console.WriteLine(x);
+		x += 8;
+		System.Console.WriteLine(x);
+		x -= 7;
+		System.Console.WriteLine(x);
+		x /= 3;
+		System.Console.WriteLine(x);
+		x *= 7;
+		System.Console.WriteLine(x);
+		x %= 9;
+		System.Console.WriteLine(x);
+		x += 1;
+		System.Console.WriteLine(x);
+	}
+}
