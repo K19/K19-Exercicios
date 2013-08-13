94b16ddf4c977fd450ef7ed4c814aa3ec2776b15
Rafael Cosentino <rafael.cosentino@k19.com.br>
2013-08-12 22:33:19 -0300

    k01-operadores-complementar15

---
+++ rafael/operadores/UseConcatenacao.cs
@@ -0,0 +1,16 @@
+class UseConcatenacao 
+{
+	static void Main() 
+	{
+		string s1 = "Rafael";
+		string s2 = "Jonas";
+		string s3 = "Marcelo";
+		string s4 = "Cosentino";
+		string s5 = "Hirata";
+		string s6 = "Martins";
+		
+		System.Console.WriteLine(s1 + " " + s4);
+		System.Console.WriteLine(s2 + " " + s5);
+		System.Console.WriteLine(s3 + " " + s6);
+	}
+}
