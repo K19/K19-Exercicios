163f6e681934e576eee9bf602191efdb0b138d50
Rafael Cosentino <rafael.cosentino@k19.com.br>
2013-08-12 22:15:53 -0300

    k01-operadores-complementar1

---
+++ rafael/operadores/UseOperadoresAritmeticos.java
@@ -0,0 +1,15 @@
+class UseOperadoresAritmeticos {
+	public static void main(String[] args) {
+		int x = 3 + 8;
+		int y = 7 - 3;
+		int z = 4 * 3;
+		int q = 8 / 2;
+		int w = 9 % 4;
+		
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+		System.out.println(q);
+		System.out.println(w);
+	}
+}
