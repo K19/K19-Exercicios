e9fa98a783e069a6eb1e6dc565a2c5fc6fb87a9e
Rafael Cosentino <rafael.cosentino@k19.com.br>
2013-08-12 22:32:29 -0300

    k01-operadores-complementar12

---
+++ rafael/operadores/UseOperadoresAritmeticos.cs
@@ -0,0 +1,17 @@
+class UseOperadoresAritmeticos 
+{
+	static void Main()
+	{
+		int x = 3 + 8;
+		int y = 7 - 3;
+		int z = 4 * 3;
+		int q = 8 / 2;
+		int w = 9 % 4;
+		
+		System.Console.WriteLine(x);
+		System.Console.WriteLine(y);
+		System.Console.WriteLine(z);
+		System.Console.WriteLine(q);
+		System.Console.WriteLine(w);
+	}
+}
