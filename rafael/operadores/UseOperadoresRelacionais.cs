ce1becfe82a5bdf546bce497c2f3c54938b3d486
Rafael Cosentino <rafael.cosentino@k19.com.br>
2013-08-12 22:34:26 -0300

    k01-operadores-complementar18

---
+++ rafael/operadores/UseOperadoresRelacionais.cs
@@ -0,0 +1,15 @@
+class UseOperadoresRelacionais 
+{
+	static void Main() 
+	{
+		int x = 20;
+		int y = 15;
+
+		System.Console.WriteLine(x > y);
+		System.Console.WriteLine(x >= y);
+		System.Console.WriteLine(x < y);
+		System.Console.WriteLine(x <= y);
+		System.Console.WriteLine(x == y);
+		System.Console.WriteLine(x != y);
+	}
+}
