921c2d4dd22423120e19c1d0143593d15674c21f
Rafael Cosentino <rafael.cosentino@k19.com.br>
2013-08-12 22:28:15 -0300

    k01-operadores-complementar4

---
+++ rafael/operadores/UseConcatenacao.java
@@ -0,0 +1,14 @@
+class UseConcatenacao {
+	public static void main(String[] args) {
+		String s1 = "Rafael";
+		String s2 = "Jonas";
+		String s3 = "Marcelo";
+		String s4 = "Cosentino";
+		String s5 = "Hirata";
+		String s6 = "Martins";
+		
+		System.out.println(s1 + " " + s4);
+		System.out.println(s2 + " " + s5);
+		System.out.println(s3 + " " + s6);
+	}
+}
