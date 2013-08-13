3bcd6d3702a8950e46159e502bb29358218b7655
Rafael Cosentino <rafael.cosentino@k19.com.br>
2013-08-12 22:30:13 -0300

    k01-operadores-complementar7

---
+++ rafael/operadores/UseOperadoresRelacionais.java
@@ -0,0 +1,13 @@
+class UseOperadoresRelacionais {
+	public static void main(String[] args) {
+		int x = 20;
+		int y = 15;
+
+		System.out.println (x > y);
+		System.out.println (x >= y);
+		System.out.println (x < y);
+		System.out.println (x <= y);
+		System.out.println (x == y);
+		System.out.println (x != y);
+	}
+}
