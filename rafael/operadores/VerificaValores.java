a321a2434da9f062c7637ca635642d952d68ed31
Rafael Cosentino <rafael.cosentino@k19.com.br>
2013-08-12 22:31:01 -0300

    k01-operadores-complementar8

---
+++ rafael/operadores/VerificaValores.java
@@ -0,0 +1,8 @@
+class VerificaValores {
+	public static void main(String [] args) {
+		int anoTorreGemeas = 2001;
+		int anoMichaelJackson = 2009;
+				
+		System.out.println((anoTorreGemeas+19)/4 >= (anoMichaelJackson+129)/5);		
+	}
+}
