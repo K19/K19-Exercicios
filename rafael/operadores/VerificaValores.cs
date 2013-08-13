175d984c8728239b526fd92dfd7b0f8c792de5d5
Rafael Cosentino <rafael.cosentino@k19.com.br>
2013-08-12 22:34:43 -0300

    k01-operadores-complementar19

---
+++ rafael/operadores/VerificaValores.cs
@@ -0,0 +1,10 @@
+class VerificaValores
+{
+	static void Main()
+	{
+		int anoTorreGemeas = 2001;
+		int anoMichaelJackson = 2009;
+				
+		System.Console.WriteLine((anoTorreGemeas+19)/4 >= (anoMichaelJackson+129)/5);
+	}
+}
