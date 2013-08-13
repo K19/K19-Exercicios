8616ae44210cdd179b08fe641839dd5124311f25
Rafael Cosentino <rafael.cosentino@k19.com.br>
2013-08-12 22:32:51 -0300

    k01-operadores-complementar13

---
+++ rafael/operadores/IdadeMedia.cs
@@ -0,0 +1,13 @@
+class IdadeMedia
+{
+	static void Main()
+	{
+		double idadeDoRafael = 27;
+		double idadeDoJonas = 29;
+		double idadeDoMarcelo = 27;
+		
+		double idadeMedia = (idadeDoRafael + idadeDoJonas + idadeDoMarcelo)/3;
+		
+		System.Console.WriteLine("Idade Média: " + idadeMedia);		
+	}
+}
