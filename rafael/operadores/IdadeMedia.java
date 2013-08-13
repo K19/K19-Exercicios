99d19037a6b666545f5d11461578c7534d71ecab
Rafael Cosentino <rafael.cosentino@k19.com.br>
2013-08-12 22:26:40 -0300

    k01-operadores-complementar2

---
+++ rafael/operadores/IdadeMedia.java
@@ -0,0 +1,11 @@
+class IdadeMedia {
+	public static void main(String[] args) {
+		double idadeDoRafael = 27;
+		double idadeDoJonas = 29;
+		double idadeDoMarcelo = 27;
+		
+		double idadeMedia = (idadeDoRafael + idadeDoJonas + idadeDoMarcelo)/3;
+		
+		System.out.println("Idade Média: " + idadeMedia);
+	}
+}
