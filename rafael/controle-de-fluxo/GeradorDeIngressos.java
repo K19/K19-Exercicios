ed5b2e889035e94d4009dc597cd9029fe426caac
Rafael Cosentino <rafael.cosentino@k19.com.br>
2013-08-22 16:12:01 -0300

    k01-controle-de-fluxo-fixacao22

---
+++ rafael/controle-de-fluxo/GeradorDeIngressos.java
@@ -0,0 +1,9 @@
+class GeradorDeIngressos {
+	public static void main(String[] args) {
+		for(int i = 1; i <= 4; i++) {
+			for(int j = 1; j <= 20; j++) {
+				System.out.println("Setor: " + i + " Cadeira: " + j);
+			}
+		}
+	}
+}
