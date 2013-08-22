aeeb0234aba3a1af7fcc6fb8c90ff7fc2b13954a
Rafael Cosentino <rafael.cosentino@k19.com.br>
2013-08-22 18:27:49 -0300

    k01-controle-de-fluxo-complementar18

---
+++ rafael/controle-de-fluxo/TresLosangosDeAstericos.cs
@@ -0,0 +1,17 @@
+class TresLosangosDeAstericos
+{
+	static void Main()
+	{
+		for(int i = 0; i < 3; i++)
+		{
+			String s1 = "";
+			String s2 = "**********";
+			for(int i = 0; i < 5; i++)
+			{
+				System.Console.WriteLine(s1);
+				System.Console.WriteLine(s2);
+				s += " ";
+			}
+		}
+	}
+}
