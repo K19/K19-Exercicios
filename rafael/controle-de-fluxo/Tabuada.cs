e827e725158bb3086a52b421df2e755724216875
Rafael Cosentino <rafael.cosentino@k19.com.br>
2013-08-22 18:28:30 -0300

    k01-controle-de-fluxo-complementar20

---
+++ rafael/controle-de-fluxo/Tabuada.cs
@@ -0,0 +1,13 @@
+class Tabuada
+{
+	static void Main()
+	{
+		for (int i = 1; i <= 10; i++)
+		{
+			for (int j = 1; j <= 10; j++)
+			{
+				System.Console.WriteLine(i + "x" + j + " = " + i * j);
+			}
+		}
+	}
+}
