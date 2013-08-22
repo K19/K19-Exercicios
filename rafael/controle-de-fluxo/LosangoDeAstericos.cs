d07939c3198b6087f98c2581ae412cd1e1c28000
Rafael Cosentino <rafael.cosentino@k19.com.br>
2013-08-22 18:27:27 -0300

    k01-controle-de-fluxo-complementar17

---
+++ rafael/controle-de-fluxo/LosangoDeAstericos.cs
@@ -0,0 +1,14 @@
+class LosangoDeAstericos
+{
+	static void Main()
+	{
+		string s1 = "";
+		string s2 = "**********";
+		for(int i = 0; i < 5; i++)
+		{
+			System.Console.Write(s1);
+			System.Console.WriteLine(s2);
+			s1 += " ";
+		}
+	}
+}
