b12fc3da9d8f7c03b472318a4850efa5d5e2a36b
Rafael Cosentino <rafael.cosentino@k19.com.br>
2013-08-22 18:24:09 -0300

    k01-controle-de-fluxo-complementar6

---
+++ rafael/controle-de-fluxo/TresLosangosDeAstericos.java
@@ -0,0 +1,13 @@
+class TresLosangosDeAstericos {
+	public static void main(String[] args) {
+		for(int i = 0; i < 3; i++) {
+			String s1 = "";
+			String s2 = "**********";
+			for(int j = 0; j < 5; j++) {
+				System.out.print(s1);
+				System.out.println(s2);
+				s1 += " ";
+			}
+		}
+	}
+}
