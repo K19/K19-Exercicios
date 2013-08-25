5404c3ebe14db67ddfe85e6f652cdd7dd27a4c2d
Rafael Cosentino <rafael.cosentino@k19.com.br>
2013-08-25 14:38:58 -0300

    k01-arrays-fixacao14

---
+++ rafael/arrays/TabelaAleatoria.java
@@ -0,0 +1,17 @@
+class TabelaAleatoria {
+	public static void main(String[] args) {
+		int[][] array = new int[3][3];
+
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array[i].length; j++) {
+				array[i][j] = (int)(Math.random() * 100);
+			}
+		}
+
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array[i].length; j++) {
+				System.out.println(array[i][j]);
+			}
+		}
+	}
+}
