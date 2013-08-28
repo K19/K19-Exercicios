9f6fc5b91e4418dafa17b2cc0f7481654f3b3092
Rafael Cosentino <rafael.cosentino@k19.com.br>
2013-08-28 19:53:15 -0300

    k01-funcoes-ou-metodos-fixacao18

---
+++ rafael/funcoes-ou-metodos/IRPF.cs
@@ -0,0 +1,41 @@
+class IRPF 
+{
+	static void Main() 
+	{
+		double reposta1 = calculaIRPF(1350.57);
+		double reposta2 = calculaIRPF(2150.37);
+		double reposta3 = calculaIRPF(3378.98);
+		double reposta4 = calculaIRPF(3956.12);
+		double reposta5 = calculaIRPF(6200.15);
+
+		System.Console.WriteLine("IRPF 1: " + reposta1);
+		System.Console.WriteLine("IRPF 2: " + reposta2);
+		System.Console.WriteLine("IRPF 3: " + reposta3);
+		System.Console.WriteLine("IRPF 4: " + reposta4);
+		System.Console.WriteLine("IRPF 5: " + reposta5);
+	}
+
+	static double calculaIRPF(double renda) 
+	{
+		if(renda <= 1710.18) 
+		{
+			return 0;
+		} 
+		else if(renda <= 2563.91) 
+		{
+			return renda * 0.075 - 128.31;
+		} 
+		else if(renda <= 3418.59) 
+		{
+			return renda * 0.15 - 320.60;
+		} 
+		else if(renda <= 4271.59) 
+		{
+			return renda * 0.225 - 577.00;
+		} 
+		else 
+		{
+			return renda * 0.275 - 790.58;
+		}
+	}
+}
