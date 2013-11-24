f7d26ad45984f285d90c001f7a122018a0487365
Rafael Cosentino <rafael.cosentino@k19.com.br>
2013-11-24 17:00:42 -0200

    k02-javascript-complementar0

---
+++ javascript/public_html/array-concat.js
@@ -0,0 +1,8 @@
+var formacaoJava = ["K11","K12"];
+var formacaoJavaAvancado = ["K21","K22","K23"];
+
+var formacaoCompleta = formacaoJava.concat(formacaoJavaAvancado);
+
+for(var i = 0; i < formacaoCompleta.length; i++) {
+	console.log(formacaoCompleta[i]);
+}
---
+++ javascript/public_html/array-elementos.js
@@ -0,0 +1,4 @@
+var cursos = ["K11","K12","K21","K22","K23","K31","K32"];
+for(var i = 0; i < cursos.length; i++) {
+	console.log(cursos[i]);
+}
